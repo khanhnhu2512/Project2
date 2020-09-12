dfc0b904826967c0a110764b4d4a21f5b9bb9c25
khanhnhu2512 <luongvannhu2512@gmail.com>
2020-09-12 11:10:33 +0700

    delete carousel in list-product

--- user/views/home-list-product.php
+++ user/views/home-list-product.php
@@ -128,41 +128,7 @@
     </nav>
 
 
-    <!-- Carousel -->
-    <div id="slides " class="carousel slide" data-ride="carousel" data-interval="2500">
-        <ul class="carousel-indicators ">
-            <li data-target="#slides " data-slide-to="0 " class="active "></li>
-            <li data-target="#slides " data-slide-to="1 "></li>
-            <li data-target="#slides " data-slide-to="2 "></li>
-            <li data-target="#slides " data-slide-to="3 "></li>
-        </ul>
-        <div class="carousel-inner ">
-            <div class="carousel-item active ">
-                <img class="d-block w-100 " src="./library/images/image-bg/bg5.jpg ">
-
-            </div>
-            <div class="carousel-item ">
-                <img class="d-block w-100 " src="./library/images/image-bg/bg1.jpg ">
-
-            </div>
-            <div class="carousel-item ">
-                <img class="d-block w-100 " src="./library/images/image-bg/bg3.jpg ">
-
-            </div>
-            <div class="carousel-item ">
-                <img class="d-block w-100 " src="./library/images/image-bg/bg2.jpg ">
-
-            </div>
-            <a class="carousel-control-prev " href="#slides " role="button " data-slide="prev">
-                <span class="carousel-control-prev-icon " aria-hidden="true "></span>
-                <span class="sr-only ">Previous</span>
-            </a>
-            <a class="carousel-control-next " href="#slides " role="button " data-slide="next">
-                <span class="carousel-control-next-icon " aria-hidden="true "></span>
-                <span class="sr-only ">Next</span>
-            </a>
-        </div>
-    </div>
+    
     <!-- Body -->
     <div class="container-fluid padding">
         <div class="row welcome text-center pt-5 justify-content-center">
