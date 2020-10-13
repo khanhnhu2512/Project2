bd73e0dd8d00752c37566c3c2055f3c0109f9af3
khanhnhu2512 <luongvannhu2512@gmail.com>
2020-10-13 18:40:14 +0700

    Update mail.php

--- plugin/mail.php
+++ plugin/mail.php
@@ -18,8 +18,8 @@ function sendmail($sendTo, $mailTitle, $mailBody)
     $mail->isSMTP(); // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = 'luongkhanhnhu2512@gmail.com'; // SMTP username
-    $mail->Password = 'Khanhnhu2512'; // SMTP password
+    $mail->Username = '[your mail]'; // SMTP username
+    $mail->Password = '[password]'; // SMTP password
     $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587; // TCP port lớn connect to
     //Recipients
